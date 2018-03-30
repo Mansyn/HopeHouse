2f3bc9802ba3faea07425ba014497086d9a727da
Stephen Schaal <stephen.schaal@gmail.com>
2018-03-30 11:14:03 -0400

    account events

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { MatSnackBar } from '@angular/material';
-import { Router } from '@angular/router';
-import { AngularFireAuth } from 'angularfire2/auth';
-import * as _moment from 'moment';
-import { default as _rollupMoment } from 'moment';
-const moment = _rollupMoment || _moment;
+import { Component, OnInit } from '@angular/core'
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
+import { MatSnackBar } from '@angular/material'
+import { Router } from '@angular/router'
+import { AngularFireAuth } from 'angularfire2/auth'
+import * as _moment from 'moment'
+import { default as _rollupMoment } from 'moment'
+const moment = _rollupMoment || _moment
+import * as _ from 'lodash'
 
-import { AuthService } from '../core/auth.service';
-import { Event } from '../components/scheduler/shared/event';
-import { EventService } from '../components/scheduler/shared/event.service';
-import { Observable } from 'rxjs/Observable';
-import { User } from '../core/user';
+import { AuthService } from '../core/auth.service'
+import { Event } from '../components/scheduler/shared/event'
+import { EventService } from '../components/scheduler/shared/event.service'
+import { Observable } from 'rxjs/Observable'
+import { User } from '../core/user'
+import { Schedule } from '../schedule/shared/schedule'
+import { ScheduleService } from '../schedule/shared/schedule.service'
 
 @Component({
   selector: 'account',
@@ -25,7 +28,8 @@ export class AccountComponent {
   email: string
   password: string
   working: boolean
-  events: Event[]
+  events: Event[] = []
+  schedules: Schedule[] = []
 
   user: Observable<User>;
 
@@ -33,6 +37,7 @@ export class AccountComponent {
     private router: Router,
     public auth: AuthService,
     private eventService: EventService,
+    private scheduleService: ScheduleService,
     private fb: FormBuilder,
     public snackBar: MatSnackBar) {
     this.working = false
@@ -43,7 +48,8 @@ export class AccountComponent {
     afAuth.authState.subscribe(user => {
       console.log(user);
       if (user) {
-        this.getUserEvents(user);
+        this.getUserEvents(user)
+        this.fetchSchedules()
       }
     })
   }
@@ -51,15 +57,35 @@ export class AccountComponent {
   getUserEvents(user) {
     this.eventService.getUserEvents(user.uid)
       .snapshotChanges()
-      .subscribe(data => {
-        this.events = [];
-        let events = [];
+      .subscribe((data) => {
         data.forEach(element => {
-          var x = element.payload.toJSON();
-          x["$key"] = element.key;
-          this.events.push(x as Event);
-        });
+          var x = element.payload.toJSON()
+          x["$key"] = element.key
+          this.events.push(x as Event)
+        })
+      })
+  }
+
+  fetchSchedules() {
+    this.scheduleService.getSchedules()
+      .snapshotChanges()
+      .subscribe(data => {
+        let schedules = []
+        data.forEach(element => {
+          var y = element.payload.toJSON()
+          y['$key'] = element.key;
+          y['events'] = this.events.filter(x => x.schedule_key == element.key)
+          schedules.push(y as Schedule)
+        })
+        this.schedules = schedules
       });
+  }
+
+  findSchedule(key) {
+    if (this.schedules.length)
+      return ''
+
+    return this.schedules.find(x => x.$key == key)
   }
 
   formatDateTime(mom) {
